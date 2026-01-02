7fd7c3e9b5cba832451e459b38377e197238910d
sword <sword-k@sjtu.edu.cn>
2026-01-02 18:12:00 +0800

    add more test for RF

--- src/test/scala/RFSpec.scala
+++ src/test/scala/RFSpec.scala
@@ -19,13 +19,41 @@ class RFSpec extends AnyFlatSpec with ChiselScalatestTester {
       dut.io.qry1_addr.poke(0.U)
       dut.io.qry2_addr.poke(0.U)
 
+      // init
+
       dut.clock.step(1)
 
       dut.io.qry1_addr.poke(1.U)
       dut.io.new_dependence_valid.poke(true.B)
       dut.io.new_reg_id.poke(1.U)
+      dut.io.new_dependence.poke(2.U)
       dut.io.qry1_has_dependence.expect(false.B)
       dut.io.qry1_val.expect(0.U)
+
+      dut.clock.step(1)
+
+      dut.io.qry1_addr.poke(1.U)
+      dut.io.new_dependence_valid.poke(false.B)
+      dut.io.qry1_has_dependence.expect(true.B)
+
+      // dependence query should have one cycle latency
+
+      dut.clock.step(1)
+
+      dut.io.commit_valid.poke(true.B)
+      dut.io.commit_reg_id.poke(1.U)
+      dut.io.commit_rob_id.poke(2.U)
+      dut.io.commit_value.poke(3.U)
+
+      dut.clock.step(1)
+
+      dut.io.qry1_addr.poke(1.U)
+      dut.io.qry1_has_dependence.expect(false.B)
+      dut.io.qry1_val.expect(3.U)
+
+      // after commit, the dependence should be eliminated, and reg value should be updated
+
+      // TODO : add more test to check the correctness of RF
     }
   }
 }
