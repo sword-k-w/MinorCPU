f4b4009174a51898f2bf6714e1737a0f0b3e6db4
livandan <livandan@sjtu.edu.cn>
2026-01-14 21:54:46 +0800

    feat: Implement two-level-adaptive predictor.

--- src/main/scala/Predictor.scala
+++ src/main/scala/Predictor.scala
@@ -12,19 +12,25 @@ class Predictor extends Module {
     val queried_pc = Input(UInt(10.W))
     val predict_result = Output(Bool())
   })
-  val confidence = RegInit(VecInit(Seq.fill(1024)(0.U(2.W))))
+  val confidence = RegInit(VecInit(Seq.fill(4096)(0.U(2.W)))) // index: pc(12, 2) ## the_second_latest ## latest
+  val local_history = RegInit(VecInit(Seq.fill(1024)(0.U(2.W))))
 
   when (io.update_info.valid) {
-    when (io.update_info.bits.actual_result && confidence(io.update_info.bits.hashed_pc) =/= 3.U) {
-      confidence(io.update_info.bits.hashed_pc) := confidence(io.update_info.bits.hashed_pc) + 1.U
-    } .elsewhen (io.update_info.bits.actual_result === false.B && confidence(io.update_info.bits.hashed_pc) =/= 0.U) {
-      confidence(io.update_info.bits.hashed_pc) := confidence(io.update_info.bits.hashed_pc) - 1.U
+    val history = local_history(io.update_info.bits.hashed_pc)
+    val index = io.update_info.bits.hashed_pc ## history
+    when (io.update_info.bits.actual_result) {
+      when (confidence(index) =/= 3.U) {
+        confidence(index) := confidence(index) + 1.U
+      }
+      local_history(io.update_info.bits.hashed_pc) := local_history(io.update_info.bits.hashed_pc)(0) ## 1.U
+    } .otherwise {
+      when (confidence(index) =/= 0.U) {
+        confidence(index) := confidence(index) - 1.U
+      }
+      local_history(io.update_info.bits.hashed_pc) := local_history(io.update_info.bits.hashed_pc)(0) ## 0.U
     }
   }
 
-  when (confidence(io.queried_pc)(1) === 1.U) {
-    io.predict_result := true.B
-  } .otherwise {
-    io.predict_result := false.B
-  }
+  io.predict_result := confidence(io.queried_pc ## local_history(io.queried_pc))(1)
+
 }
