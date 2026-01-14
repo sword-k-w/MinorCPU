b1817c6a0ba2d4ec3d8531c0e0b7fc288c008b26
sword <sword-k@sjtu.edu.cn>
2026-01-14 20:56:07 +0800

    add broadcast accepting condition in LSQ

--- src/main/scala/LSQ.scala
+++ src/main/scala/LSQ.scala
@@ -109,11 +109,13 @@ class LSQ extends Module {
           new_entry(i.U).ready := false.B
           new_entry(i.U).dest := io.rob_tail
         }
-      } .elsewhen (io.rob_broadcast_result.valid && io.rob_broadcast_result.bits.dest === entry(i.U).dest) {
+      } .elsewhen (io.rob_broadcast_result.valid && io.rob_broadcast_result.bits.dest === entry(i).dest
+        && !entry(i).ready) {
         new_entry(i.U).ready := true.B
         new_entry(i.U).address := io.rob_broadcast_result.bits.addr
         new_entry(i.U).value := io.rob_broadcast_result.bits.value
-      } .elsewhen (io.alu_broadcast_result.valid && io.alu_broadcast_result.bits.dest === entry(i.U).dest) {
+      } .elsewhen (io.alu_broadcast_result.valid && io.alu_broadcast_result.bits.dest === entry(i).dest
+        && !entry(i).ready) {
         new_entry(i.U).ready := !io.alu_broadcast_result.bits.mmio
         new_entry(i.U).address := io.alu_broadcast_result.bits.addr
         new_entry(i.U).instruction.mmio := io.alu_broadcast_result.bits.mmio
